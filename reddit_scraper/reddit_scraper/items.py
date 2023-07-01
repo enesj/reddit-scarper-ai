a8903f83d10f1c670aa827b6778ab8327829b7fd
enesj <enes.jakic@gmail.com>
2023-07-01 21:04:44 +0200

    Revert "Build a web scraping tool using Python and Scrapy to extract data from Reddit."

--- reddit_scraper/reddit_scraper/items.py
+++
@@ -1,9 +0,0 @@
-```python
-import scrapy
-
-class ScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    title = scrapy.Field()
-    url = scrapy.Field()
-    comments = scrapy.Field()
-```
